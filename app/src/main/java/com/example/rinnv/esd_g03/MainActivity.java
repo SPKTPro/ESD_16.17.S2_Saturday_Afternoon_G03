eef2e102524289a83894664e584883bed974fec0
linh vo <linhvo1008@gmail.com>
2017-04-19 09:00:45 +0700

    main activity

--- app/src/main/java/com/example/rinnv/esd_g03/MainActivity.java
+++ app/src/main/java/com/example/rinnv/esd_g03/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.rinnv.esd_g03;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button couple1 = (Button) findViewById(R.id.button1);
+        Button couple2 = (Button) findViewById(R.id.button2);
+        couple1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
